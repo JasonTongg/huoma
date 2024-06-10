b66bfd096d1265c5f65e61bad6f656ba11674853
jasontongg <aboytong123@gmail.com>
2024-06-10 12:54:18 +0700

    coming soon pagee

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Logo from "../public/logo.png";
+import Image from "next/image";
 
 export default function index() {
-  return <div>Meimei coin</div>;
+  return (
+    <div className="background-china w-full h-screen flex items-center justify-center flex-col gap-2">
+      <Image src={Logo} className="w-[400px]"></Image>
+      <h1 className="text-white text-5xl font-bold gang-of-three tracking-widest">
+        Meimei Token is Launching Soon!
+      </h1>
+    </div>
+  );
 }
