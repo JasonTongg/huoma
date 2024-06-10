3a7abf991205c04cf54839c1134fbce3f3305252
jasontongg <aboytong123@gmail.com>
2024-06-10 20:21:04 +0700

    responsive

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import House from "../public/house.svg";
 import CoinLeft from "../public/CoinLeft.png";
 import CoinRight from "../public/CoinRight.png";
 import Letter from "../public/letter.png";
+import Gold1 from "../public/gold1.png";
 
 export default function Index() {
   // const [topPosition1, setTopPosition1] = useState(0);
@@ -29,22 +30,36 @@ export default function Index() {
   const [peiLeft, setPeiLeft] = useState(30);
   const [coinLeftTop, setCoinLeftTop] = useState(20);
   const [coinRightTop, setCoinRightTop] = useState(20);
-  const [meipeimin, setMeiPeiMin] = useState(25);
 
   useEffect(() => {
+    let min = 400;
+    if (window.innerWidth < 768) {
+      min = 100;
+    }
     const handleScroll = () => {
-      if (window.scrollY > 400 && window.scrollY < 1400) {
+      if (window.scrollY > min && window.scrollY < 1400) {
         let meipeispeed = 20;
-        if (window.innerWidth < 1500) {
+        let meipeimin = min / 16;
+        let minpeileftspeed = 60;
+        if (window.innerWidth < 360) {
+          meipeispeed = 7;
+          minpeileftspeed = 14;
+        } else if (window.innerWidth < 640) {
+          meipeispeed = 10;
+          minpeileftspeed = 25;
+        } else if (window.innerWidth < 768) {
+          meipeispeed = 12;
+          minpeileftspeed = 30;
+        } else if (window.innerWidth < 1024) {
+          meipeispeed = 16;
+        } else if (window.innerWidth < 1500) {
           meipeispeed = 18;
         }
-        if (window.innerWidth < 1024) {
-          meipeispeed = 15.5;
-        }
+
         setMeiTop(meiTop + window.scrollY / meipeispeed - meipeimin);
         setPeiTop(peiTop + window.scrollY / meipeispeed - meipeimin);
-        setPeiLeft(peiLeft - window.scrollY / 60 + 6);
-        setCoinLeft(coinLeft - window.scrollY / 60 + 6);
+        setPeiLeft(peiLeft - window.scrollY / minpeileftspeed + 6);
+        setCoinLeft(coinLeft - window.scrollY / minpeileftspeed + 6);
       }
     };
 
@@ -63,7 +78,7 @@ export default function Index() {
 
   return (
     <div className="bg-[#F20C00] min-h-screen flex flex-col items-center justify-start gap-2">
-      <div className="flex items-center justify-center gap-2 gang-of-three sticky top-0 left-0 w-[103vw] z-[10] -translate-x-1 md:-translate-x-4 bg-[#FBD406] text-center p-2">
+      <div className="flex text-xs md:text-2xl items-center justify-center gap-2 break-all gang-of-three sticky top-0 left-0 w-[103vw] z-[10] -translate-x-1 md:-translate-x-4 bg-[#FBD406] text-center p-2 px-4">
         0x0000000000000000000000000000000000000000{" "}
         <MdOutlineContentCopy
           className="cursor-pointer"
@@ -97,29 +112,39 @@ export default function Index() {
         </div>
       </nav>
       <main className="flex items-center justify-center gap-2 flex-col w-full relative overflow-hidden">
-        <div className="w-full scrollContainer bg-house h-[1150px] [&>*]:absolute">
+        <div className="w-full scrollContainer bg-house h-[500px] sm:h-[700px] md:h-[1150px] [&>*]:absolute">
           <Image
             src={MeiMei}
-            className="w-[150px] lg:w-[200px] 2xl:w-[280px] z-[4]"
+            className="w-[80px] sm:w-[110px] md:w-[150px] lg:w-[200px] 2xl:w-[280px] z-[4]"
             style={{ top: `${meiTop}%`, right: `${peiLeft}%` }}
           />
           <Image
             src={PeiPei}
-            className="w-[170px] lg:w-[220px] 2xl:w-[300px] z-[4]"
+            className="w-[90px] sm:w-[130px] md:w-[170px] lg:w-[220px] 2xl:w-[300px] z-[4]"
             style={{ top: `${peiTop}%`, left: `${peiLeft}%` }}
           />
           <Image
             src={CoinLeft}
-            className={`w-[150px] z-[3]`}
+            className={`w-[60px] sm:w-[120px] md:w-[150px] z-[3] animate-bounce`}
             style={{ top: `${coinLeftTop}%`, left: `${coinLeft}%` }}
           />
           <Image
             src={CoinRight}
-            className={`w-[120px] z-[3]`}
+            className={`w-[60px] sm:w-[120px] md:w-[120px] z-[3] animate-bounce`}
             style={{ top: `${coinRightTop}%`, right: `${coinLeft}%` }}
           />
+          <Image
+            src={Gold1}
+            className={`w-[60px] sm:w-[120px] md:w-[120px] z-[3] animate-spin`}
+            style={{ top: `${coinRightTop + 7}%`, left: `${coinLeft}%` }}
+          />
+          <Image
+            src={Gold1}
+            className={`w-[60px] sm:w-[120px] md:w-[120px] z-[3] animate-spin`}
+            style={{ top: `${coinRightTop + 7}%`, right: `${coinLeft}%` }}
+          />
         </div>
-        <div className="w-full h-[50vw] bg-letter mt-[3rem]"></div>
+        <div className="w-full h-[65vw] sm:h-[50vw] bg-letter mt-[3rem]"></div>
       </main>
       <footer>Footer</footer>
     </div>
